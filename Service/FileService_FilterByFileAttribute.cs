7f2df10a722cb08e362a8453598a411bde97d4c4
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-07 16:54:21 +0800

    建立 FileService_FilterByFileAttribute
    1.　繼承 IFileService
    　1.1　實作方法：GetFiles

---
+++ Service/FileService_FilterByFileAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Service.Protocol;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Service
+{
+    public sealed class FileService_FilterByFileAttribute : IFileService
+    {
+        private readonly string path;
+
+        public FileService_FilterByFileAttribute(string path)
+        {
+            this.path = path;
+        }
+
+        public List<string> GetFiles()
+        {
+            return
+                new DirectoryInfo(this.path).GetFiles()
+                .Where(file =>
+                    file.Attributes.HasFlag(FileAttributes.System) == false &&
+                    file.Attributes.HasFlag(FileAttributes.Hidden) == false)
+                .Select(file => file.FullName)
+                .ToList();
+        }
+    }
+}
