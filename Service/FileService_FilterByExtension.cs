97f795bd92de65ed12ccc969d658a24e1e425595
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-07 16:02:17 +0800

    建立 FileService_FilterByExtension
    1.　繼承 IFileService
    　1.1　實作方法：GetFiles

---
+++ Service/FileService_FilterByExtension.cs
@@ -0,0 +1,25 @@
+﻿using Service.Protocol;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Service
+{
+    public sealed class FileService_FilterByExtension : IFileService
+    {
+        private readonly string path;
+
+        public FileService_FilterByExtension(string path)
+        {
+            this.path = path;
+        }
+
+        public List<string> GetFiles()
+        {
+            return
+                Directory.GetFiles(this.path)
+                .Where(file => ".db".Equals(Path.GetExtension(file).ToLower()) == false)
+                .ToList();
+        }
+    }
+}
