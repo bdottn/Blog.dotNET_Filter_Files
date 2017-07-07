8e292e65d6c2d88c5bb0292385b063fedf3e09f5
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-07 15:43:23 +0800

    建立 FileService_FilterBySearchPattern
    1.　繼承 IFileService
    　1.1　實作方法：GetFiles

---
+++ Service/FileService_FilterBySearchPattern.cs
@@ -0,0 +1,22 @@
+﻿using Service.Protocol;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Service
+{
+    public sealed class FileService_FilterBySearchPattern : IFileService
+    {
+        private readonly string path;
+
+        public FileService_FilterBySearchPattern(string path)
+        {
+            this.path = path;
+        }
+
+        public List<string> GetFiles()
+        {
+            return Directory.GetFiles(this.path, "*.jpg").ToList();
+        }
+    }
+}
