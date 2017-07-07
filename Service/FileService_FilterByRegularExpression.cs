186e5a9af8a8bf905cb051586e15794454edde69
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-07 16:45:40 +0800

    建立 FileService_FilterByRegularExpression
    1.　繼承 IFileService
    　1.1　實作方法：GetFiles

---
+++ Service/FileService_FilterByRegularExpression.cs
@@ -0,0 +1,28 @@
+﻿using Service.Protocol;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Service
+{
+    public sealed class FileService_FilterByRegularExpression : IFileService
+    {
+        private readonly string path;
+
+        public FileService_FilterByRegularExpression(string path)
+        {
+            this.path = path;
+        }
+
+        public List<string> GetFiles()
+        {
+            var pattern = @"^(.)*.(db|DB)$";
+
+            return
+                Directory.GetFiles(this.path)
+                .Where(file => new Regex(pattern).IsMatch(Path.GetFileName(file)) == false)
+                .ToList();
+        }
+    }
+}
