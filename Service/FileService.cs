caf83399d46ebd19f8ec32a4c809a950eb66f154
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-07 15:33:41 +0800

    建立 FileService
    1.　繼承 IFileService
    　1.1　實作方法：GetFiles

---
+++ Service/FileService.cs
@@ -0,0 +1,22 @@
+﻿using Service.Protocol;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Service
+{
+    public sealed class FileService : IFileService
+    {
+        private readonly string path;
+
+        public FileService(string path)
+        {
+            this.path = path;
+        }
+
+        public List<string> GetFiles()
+        {
+            return Directory.GetFiles(this.path).ToList();
+        }
+    }
+}
