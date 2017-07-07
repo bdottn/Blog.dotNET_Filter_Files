350281fc457f5f6e46fc81d9308b1544b4f9f50b
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-07 16:02:30 +0800

    建立 FileService_FilterByExtensionTests 單元測試
    1.　加入測試案例：GetFilesTest_預期取得檔案路徑

---
+++ ServiceUnitTest/FileService_FilterByExtensionTests.cs
@@ -0,0 +1,49 @@
+﻿using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Service.Protocol;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Service.UnitTest
+{
+    [TestClass]
+    public sealed class FileService_FilterByExtensionTests
+    {
+        private IFileService service;
+
+        private string testFolderPath;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.testFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "ONE_PIECE");
+
+            // 將 Thumbs.db 設為系統檔、隱藏檔
+            var thumbsPath = Path.Combine(this.testFolderPath, "Thumbs.db");
+            File.SetAttributes(thumbsPath, FileAttributes.System);
+            File.SetAttributes(thumbsPath, FileAttributes.Hidden);
+
+            this.service = new FileService_FilterByExtension(this.testFolderPath);
+        }
+
+        [TestMethod]
+        public void GetFilesTest_預期取得檔案路徑()
+        {
+            var expected =
+                new List<string>()
+                {
+                    Path.Combine(this.testFolderPath, "01_魯夫.jpg"),
+                    Path.Combine(this.testFolderPath, "02_索隆.jpg"),
+                    Path.Combine(this.testFolderPath, "03_騙人布.jpg"),
+                    Path.Combine(this.testFolderPath, "04_娜美.jpg"),
+                    Path.Combine(this.testFolderPath, "05_香吉士.jpg"),
+                    Path.Combine(this.testFolderPath, "06_喬巴.jpg"),
+                    Path.Combine(this.testFolderPath, "07_羅賓.jpg"),
+                };
+
+            var actual = this.service.GetFiles();
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+    }
+}
